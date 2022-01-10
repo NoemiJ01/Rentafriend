2ebce41e3dbd031495f8183df5eb93c143e3cb72
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-01-10 12:04:13 -0600

    Initial commit

---
+++ src/components/splash.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/splash.css";
+
+function Splash() {
+  return (
+    <div className="Splash">
+      <h1 className="primary text">Rent A Friend</h1>
+      <h5 className="secondary text">
+        Money Can't buy happieness but it can by friends. What's in your
+        wallet?🤠
+      </h5>
+    </div>
+  );
+}
+
+export default Splash;
